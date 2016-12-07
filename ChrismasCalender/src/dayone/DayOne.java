ca148d02bbfe95e86ac0819f7cb79c701e6786bc
Morten J <morten2094@hotmail.com>
2016-12-08 00:38:05 +0100

    day 1 part two

--- ChrismasCalender/src/dayone/DayOne.java
+++ ChrismasCalender/src/dayone/DayOne.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 
 public class DayOne {
 
-	String[] array;
-	int x = 0, y = 0;
-	int degree = 2; // 3 right, 2 north, 1 left, 0 south
-	ArrayList<Point> points;
+	private String[] array;
+	private int x = 0, y = 0;
+	private int degree = 2; // 3 right, 2 north, 1 left, 0 south
+	private ArrayList<Point> points;
+	private boolean foundPartTwo = false;
 	public DayOne(String[] array){
 		this.array = array;
 		points = new ArrayList<Point>();
@@ -32,15 +33,27 @@ public class DayOne {
 			int amount = Integer.parseInt(array[i].substring(1, array[i].length()));
 			switch(degree){
 			case 0:
+				for(int f = 0; f < amount; f++){
+					points.add(new Point(x, y-f));
+				}
 				y-= amount;
 				break;
 			case 1:
+				for(int f = 0; f < amount; f++){
+					points.add(new Point(x-f, y));
+				}
 				x-= amount;
 				break;
 			case 2:
+				for(int f = 0; f < amount; f++){
+					points.add(new Point(x, y+f));
+				}
 				y+= amount;
 				break;
 			case 3:
+				for(int f = 0; f < amount; f++){
+					points.add(new Point(x+f, y));
+				}
 				x+= amount;
 				break;
 			}
@@ -52,7 +65,26 @@ public class DayOne {
 			y = -y;
 		}
 		System.out.println("Distance: "+(x+y));
-		System.out.println("Part two Distance: ");
+		for(int i = 0; i < points.size(); i++){
+			for(int f = 0; f < points.size(); f++){
+				if(f != i){
+					if(points.get(i).x == points.get(f).x){
+						if(points.get(i).y == points.get(f).y){
+							x = points.get(i).x;
+							y = points.get(i).y;
+							if(x < 0){
+								x = -x;
+							}
+							if(y < 0){
+								y = -y;
+							}
+							System.out.println("Part two distance: "+(x+y));
+							return;
+						}
+					}
+				}
+			}
+		}
 	}
 	
 }
